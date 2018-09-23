44afeed84219e8b17cfcdb12422de959315e10d7
Ma-zhengyang <373591737@qq.com>
2018-09-23 13:11:22 +0800

    add starIndicator shadoww

--- app/src/main/java/com/example/mzy/loadinganimations/indicator/Star/StarIndicator.java
+++ app/src/main/java/com/example/mzy/loadinganimations/indicator/Star/StarIndicator.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.CornerPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.Log;
 import android.view.animation.DecelerateInterpolator;
 
@@ -19,15 +20,17 @@ import java.util.ArrayList;
 
 public class StarIndicator extends IndicatorDrawable {
 
+    private static final float THICKNESS = 0.7f; //星的胖度系数
     private final String TAG = StarIndicator.class.getSimpleName();
 
     //正多边形每个内角相等，度数为（n-2）*180/n，n为边数
-
     private float starHeight;
-    private StarModel mStarModel = new StarModel(0.7f);
+    private StarModel mStarModel = new StarModel(THICKNESS);
     private CornerPathEffect pathEffect = new CornerPathEffect(30);
 
     private float mAnimatedValue;
+    private RectF mShadowRect = new RectF();
+    private float mShadowWidth;
 
     public StarIndicator(Context context) {
         Log.d(TAG, "StarIndicator: ");
@@ -35,12 +38,13 @@ public class StarIndicator extends IndicatorDrawable {
         init();
 
         starHeight = dip2px(mContext, 20.0f);
+        mStarModel.setDrawingOuterRect(0, 0, starHeight);
+
+        mShadowWidth = dip2px(mContext, 10.0f);
     }
 
     @Override
     protected ArrayList<ValueAnimator> getAnimation() {
-
-        mStarModel.setDrawingOuterRect(0, 0, starHeight);
 
         ArrayList<ValueAnimator> list = new ArrayList<>();
 
@@ -65,17 +69,24 @@ public class StarIndicator extends IndicatorDrawable {
     protected void draw(Canvas canvas, Paint paint) {
 
         canvas.save();
-        if (mAnimatedValue < 0.5) {//下落
-            // 起始位置getHeight() / 4，终点 getHeight() / 2
-            canvas.translate(getWidth() / 2, getHeight() / 4 + mAnimatedValue * (getHeight() / 2));
+
+        float x = getWidth() / 2 - starHeight / 2;
+        float value;
+        if (mAnimatedValue <= 0.5) {//下落
+            value = mAnimatedValue;
         } else {//弹起
-            canvas.translate(getWidth() / 2, getHeight() / 4 + (1 - mAnimatedValue) * (getHeight() / 2));
+            value = 1 - mAnimatedValue;
         }
+        canvas.translate(x, getHeight() / 4 + value * starHeight);
 
         //以五角星中心位置为旋转原点
         canvas.rotate(360 * mAnimatedValue, starHeight / 2, starHeight / 2);
         drawStar(canvas, paint);
+
         canvas.restore();
+
+        //恢复画布后再画阴影，否则阴影也会旋转
+        drawShadow(canvas, paint);
     }
 
     private void drawStar(Canvas canvas, Paint paint) {
@@ -115,4 +126,23 @@ public class StarIndicator extends IndicatorDrawable {
         canvas.drawPath(path, paint);
     }
 
+    private void drawShadow(Canvas canvas, Paint paint) {
+
+        float value;
+        if (mAnimatedValue <= 0.5) {//下落
+            value = mAnimatedValue;
+        } else {//弹起
+            value = 1 - mAnimatedValue;
+        }
+
+        //getHeight() / 4 + 0.5f * starHeight为星的top那个角的y坐标
+        float y = getHeight() / 4 + 0.5f * starHeight + starHeight;
+
+        mShadowRect.set(getWidth() / 2 - value * mShadowWidth - 10,
+                y,
+                getWidth() / 2 + value * mShadowWidth + 10,
+                y + 8);
+
+        canvas.drawOval(mShadowRect, paint);
+    }
 }
